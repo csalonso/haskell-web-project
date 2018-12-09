50fc95f944e395a754d8606c3d3fd81689de78b7
Caio Alonso <s.alonso.caio@gmail.com>
2018-12-09 20:46:27 +0000

    [CAIO] Cadastrar piloto via formulário

--- src/Handler/MainPiloto.hs
+++ src/Handler/MainPiloto.hs
@@ -20,5 +20,28 @@ getMainPilotoR = do
         toWidget $(luciusFile "templates/mainPiloto.lucius")
         $(whamletFile "templates/mainPiloto.hamlet")
         
+getEditPilotoR :: Handler Html
+getEditPilotoR = do 
+    (widgetForm, enctype) <- generateFormPost formPiloto
+    defaultLayout $ do 
+        addStylesheet $ StaticR css_bootstrap_css
+        toWidget $(luciusFile "templates/editPiloto.lucius")
+        $(whamletFile "templates/editPiloto.hamlet")        
+        
+formPiloto :: Form (Piloto)
+formPiloto = renderBootstrap $ Piloto
+    <$> areq textField "Nome: " Nothing
+    <*> areq dayField "Data de Nascimento: " Nothing
+    <*> areq intField "Peso (Kg): " Nothing
+    <*> areq intField "Altura (Cm): " Nothing
+    
+postEditPilotoR :: Handler Html 
+postEditPilotoR = do 
+    ((res,_),_) <- runFormPost formPiloto
+    case res of 
+        FormSuccess (piloto) -> do
+                runDB $ insert piloto
+                redirect MainPilotoR
+        _ -> redirect EditPilotoR
         
     
