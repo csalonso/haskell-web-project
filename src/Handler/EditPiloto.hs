cb812557d8b7afdec95f7da78b2a18213d44af5b
Caio Alonso <s.alonso.caio@gmail.com>
2018-12-09 12:43:54 +0000

    [DIEGO] Criar base das telas de edição de perfil (HTML)

---
+++ src/Handler/EditPiloto.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+module Handler.MainPiloto where
+
+import Text.Lucius
+import Text.Julius
+import Import
+import Prelude
+
+widgetFooter :: Widget
+widgetFooter = $(whamletFile "templates/footer.hamlet")
+
+getMainPilotoR :: Handler Html
+getMainPilotoR = do 
+    defaultLayout $ do 
+        addStylesheet $ StaticR css_bootstrap_css
+        toWidget $(luciusFile "templates/mainPiloto.lucius")
+        $(whamletFile "templates/mainPiloto.hamlet")
+        
+        
+    
